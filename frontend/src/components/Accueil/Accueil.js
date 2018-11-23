01e265bfe38827d588f705c50b5de26c6fccd1bb
Hugo Arvis <arvishugo@hotmail.fr>
2018-11-23 11:30:42 +0100

    recupp

--- frontend/src/components/Accueil/Accueil.js
+++ frontend/src/components/Accueil/Accueil.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import'./Accueil.scss';
+
 
 export default class Accueil extends Component {
   render() {
     return (
-      <div>
+      <div className="backgroundAccueil">
+          <div className="opacityOrange">
+              <p>Le bon contenu au bon endroit au bon moment</p>
+
+              <p>Walkoo combine la géolocalisation et les sciences du comportement 
+                pour renforcer l'engagement de l'utilisateur en maîtrisant le sens 
+                du contexte</p>
+
+              <button>Demander une Demo</button>
+              
+          
+          </div>
         
       </div>
     )
