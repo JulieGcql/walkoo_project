cb54807e5228424b7de2d94db79baf6fac42b2c0
Julien <julien.bertocchini@gmail.com>
2018-11-27 11:58:46 +0100

    Create Technologie component + SCSS + Responsive

--- frontend/src/Pages/Website/Website.js
+++ frontend/src/Pages/Website/Website.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import Expertise from '../../components/Expertise/Expertise'
+import Technologie from '../../components/Technologie/Technologie'
 
 export default class Website extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class Website extends Component {
       <div>
         <Navbar/>
         <Expertise/>
+        <Technologie/>
       </div>
     )
   }
