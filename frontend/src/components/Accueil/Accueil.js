9d79d4c69bbb0931185ef288dcc68e6a4b37c734
Hugo Arvis <arvishugo@hotmail.fr>
2018-11-27 16:45:46 +0100

    accueil with grid

--- frontend/src/components/Accueil/Accueil.js
+++ frontend/src/components/Accueil/Accueil.js
@@ -5,20 +5,26 @@ import'./Accueil.scss';
 export default class Accueil extends Component {
   render() {
     return (
-      <div className="backgroundAccueil">
-          <div className="opacityOrange">
-              <p>Le bon contenu au bon endroit au bon moment</p>
-
-              <p>Walkoo combine la géolocalisation et les sciences du comportement 
-                pour renforcer l'engagement de l'utilisateur en maîtrisant le sens 
-                du contexte</p>
-
-              <button>Demander une Demo</button>
-              
-          
-          </div>
-        
+      <div class="Container">
+        <div className="opacity">
+          <div class="Text1">Le bon contenu au bon endroit au bon moment</div>
+          <div class="Text2">Walkoo combine la géolocalisation et les sciences du comportement
+                      pour renforcer l'engagement de l'utilisateur en maîtrisant le sens
+                du contexte</div>
+          <div class="boutton">Demander une Demo</div>
+        </div>
       </div>
+    
+    
     )
   }
 }
+
+
+
+
+
+
+
+
+
