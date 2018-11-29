ddd2c9ce13cb51a84e232b612e69d44ab0ff2a0d
Maxime <Maxime.pochet@hotmail.fr>
2018-11-29 21:17:03 +0100

    ADD SWIPE ON SECTOR COMPONENT
    add react-slick library for the swipe sectors section on the mobile device, and changed padding
    for the navbar

--- frontend/src/Pages/Website/Website.js
+++ frontend/src/Pages/Website/Website.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import Expertise from '../../components/Expertise/Expertise'
 import Technologie from '../../components/Technologie/Technologie'
--- frontend/src/components/Secteurs/SecteursMobile.js
+++ frontend/src/components/Secteurs/SecteursMobile.js
@@ -1,51 +1,41 @@
 import React, { Component } from 'react'
 import SectorsData from './Secteurs.json'
-import Back from './pictures/back.png'
-import Next from './pictures/next.png'
+import Slider from 'react-slick'
 import './SecteursMobile.scss'
 
 export default class SecteursMobile extends Component {
   state = {
-    currentActive : 0
-  }
-
-
-  handleBackSector = () => {
-    if (this.state.currentActive === 0){
-      this.setState({currentActive : SectorsData.length -1})
-    } else {
-      this.setState({currentActive : this.state.currentActive - 1})
-    }
-  }
-  handleNextSector = () => {
-    if (this.state.currentActive === SectorsData.length -1){
-      this.setState({currentActive : 0})
-    } else {
-      this.setState({currentActive : this.state.currentActive + 1})
-    }
+    sectors : SectorsData
   }
 
   render() {
-    const { currentActive } = this.state;
+    const { sectors } = this.state;
+    const settings = {
+      dots: true,
+      arrows: false,
+    };
     return (
       <div className="SecteursMobileContainer">
 
         <h1 className="SecteursMobileTitle">Secteurs</h1>
 
         <div className="SecteursMobileCards">
-        <img src={Back} alt="Back" className="SecteursMobileRows" onClick={() => this.handleBackSector()}/>
-          {SectorsData.map((secteur, index) => 
-            currentActive === index &&
 
-                <div className="SecteurMobileCard" key={index}>
+        <Slider {...settings}>
 
-                  <img className="SecteurMobilePicto" src={secteur.media} alt={`Logo ${secteur.title}`}/>
-                  <h3 className="SecteurMobileTitle">{secteur.title}</h3>
-                  <p className="SecteurMobileDescription">{secteur.description}</p>
-                </div>         
-            
-          )}
-        <img src={Next} alt="Next" className="SecteursMobileRows" onClick={() => this.handleNextSector()}/>
+          {sectors.map((secteur, index) => {
+            return (
+              <div className="SecteurMobileCard" key={index}>
+
+                <img className="SecteurMobilePicto" src={secteur.media} alt={`Logo ${secteur.title}`}/>
+                <h3 className="SecteurMobileTitle">{secteur.title}</h3>
+                <p className="SecteurMobileDescription">{secteur.description}</p>
+                
+              </div>         
+            )
+          })}
+
+          </Slider>
         </div>
       </div>
     )
