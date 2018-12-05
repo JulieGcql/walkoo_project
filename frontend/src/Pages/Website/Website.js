e8644f49f5dec5478c7f52c561a2667ef36cd4d9
Julie Gicquel <julie.gcql@gmail.com>
2018-12-05 11:40:34 +0100

    correct import in website

--- frontend/src/Pages/Website/Website.js
+++ frontend/src/Pages/Website/Website.js
@@ -1,8 +1,10 @@
 import React, { Component }  from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import Expertise from '../../components/Expertise/Expertise'
+import Secteurs from '../../components/Secteurs/Secteurs'
 import Technologie from '../../components/Technologie/Technologie'
 import Realisations from '../../components/Realisations/Realisations'
+import Contact from '../../components/Contact/Contact'
 
 const Website = () => (
       <div>
