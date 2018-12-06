dbaf38d6a91f496fb86843ce88763758c5d18bb3
Hugo Arvis <arvishugo@hotmail.fr>
2018-12-06 09:47:43 +0100

    component accueil and salons et média

--- frontend/src/Pages/Website/Website.js
+++ frontend/src/Pages/Website/Website.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import Expertise from '../../components/Expertise/Expertise'
 import Accueil from '../../components/Accueil/Accueil'
+import MediasSalons from '../../components/MediasSalons/MediasSalons'
 
 export default class Website extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Website extends Component {
         <Navbar/>
         <Expertise/>
         <Accueil/>
+        <MediasSalons/>
       </div>
     )
   }
--- frontend/src/components/Accueil/Accueil.js
+++ frontend/src/components/Accueil/Accueil.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import'./Accueil.scss';
+import './Accueil.scss';
 
 
 export default class Accueil extends Component {
   render() {
     return (
-      <div class="Container">
-        <div className="opacity">
-          <div class="Text1">Le bon contenu au bon endroit au bon moment</div>
-          <div class="Text2">Walkoo combine la géolocalisation et les sciences du comportement
-                      pour renforcer l'engagement de l'utilisateur en maîtrisant le sens
-                du contexte</div>
-          <div class="boutton">Demander une Demo</div>
+      <div class="containerAnim">
+
+        <div className="Text1 ">
+
+              <div className="first">Le bon contenu</div>
+              <div className="second">Au bon endroit</div>
+              <div className="third">Au bon moment</div>
         </div>
+
+        <div className="secondContainer">
+
+          <p className="Text2">Walkoo combine la géolocalisation et les sciences du comportement pour renforcer l'engagement de l'utilisateur en maîtrisant le sens du contexte</p>
+          <p className="Bouton">Demander une Demo</p>
+        </div>
+
+
       </div>
-    
-    
+
+
     )
   }
 }
+
 
 
 
