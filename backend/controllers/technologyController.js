865dcc4bdd1b89282e42e243b964d91587eea02a
Julien <julien.bertocchini@gmail.com>
2018-12-12 09:18:22 +0100

    Create technology controler

---
+++ backend/controllers/technologyController.js
@@ -0,0 +1,67 @@
+const models = require("../models")
+const Technology = models.Technology
+
+module.exports = {
+
+    index: function(req, res, next) {
+        Technology.findAll()
+        .then((technology) => {
+          res.json({technology})
+        })
+        .catch((err) => res.json("Erreur lors de l'index de technologie", err))
+      },
+
+      create: function(req, res, next) {
+        Technology.create({
+          title : req.body.title,
+          description: req.body.description,
+          // subtitle:req.body.description,
+          mediaId : req.body.mediaId,
+          // advantageTitle: req.body.advantageTitle,
+          // advantageDescription: req.body.advantageDescription,
+
+        })
+        .then((newTechnology) => {
+          res.json({newTechnology})
+        })
+        .catch((err) => res.json("Erreur lors de la création d'un élément dans technologie", err))
+      },
+
+      edit: function(req, res, next) {
+        Technology.findByPk(req.params.id)
+        .then((technology) => {
+          if(technology){
+            if(req.body.mediaId && req.body.title && req.body.description){
+              technology.mediaId = req.body.mediaId;
+              technology.title = req.body.title;
+              technology.description = req.body.description;
+              technology.save()
+              .then((updatedTechnology) => {
+                res.json({technology: updatedTechnology})
+              })
+              .catch((error) => res.status(500).json({message: "Erreur lors de la modifiction d'un élément dans technologie."}))
+            } else {
+              res.status(500).json({message: "name cannot be blank"})
+            }
+          } else {
+            res.status(404).json({message: `Un élément dans technologie correspondant à l'id : ${req.params.id} n'existe pas.` })
+          }
+        })
+        .catch((error) => res.status(500).json({message: "Erreur lors de la recherche d'un élément dans technologie avant modification"}))
+      },
+
+      delete: function(req, res, next) {
+        Technology.findByPk(req.params.id)
+        .then((technology) => {
+          if(technology){
+            technology.destroy()
+            .then((technology) => res.json({message: "Un élément dans technologie a été supprimé."}))
+            .catch((err) => res.status(500).json({message: "Erreur lors de la suppression d'un élément dans technologie."}))
+          } else {
+            res.status(404).json({message : `Un élément dans technologie correspondant à l'id : ${req.params.id} n'existe pas.`})
+          }
+        })
+        .catch((err) => res.status(500).json({message: "Erreur lors de la recherche d'un élément dans technologie avant suppression."}))
+      }
+
+}
