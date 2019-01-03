f6f7e7842900a8676286c6dac4dc4e3ee47e09c9
Julie Gicquel <julie.gcql@gmail.com>
2019-01-03 16:04:13 +0100

    modif back expertise + join with front

--- frontend/src/components/BackOffice/AdminArticles/Expertise/Expertise.js
+++ frontend/src/components/BackOffice/AdminArticles/Expertise/Expertise.js
@@ -12,11 +12,6 @@ export default class Expertise extends Component {
     mediaId:"",
     paragraphOne:"",
     paragraphTwo:"",
-    // modal: false,
-    // idSelected:"",
-    // mediaIdSelected:"",
-    // paragraphOneSelected:"",
-    // paragraphTwoSelected:""
   }
 
   componentDidMount() {
@@ -32,38 +27,10 @@ export default class Expertise extends Component {
     this.setState({mediaId: id})
   }
 
-  // handleDelete = (id) => {
-  //   if(window.confirm("Voulez-vous supprimer l'expertise ?")){
-  //     axios.delete(`/expertise/delete/${id}`)
-  //     .then((res) => {
-  //       this.getExpertise()
-  //     })
-  //     .catch((err) => console.log(err))
-  //   }
-  // }
-
-  // handleClick = (id, mediaId, paragraphOne, paragraphTwo) => {
-  //   this.setState({modal: !this.state.modal, idSelected: id, mediaIdSelected:mediaId, paragraphOneSelected: paragraphOne, paragraphTwoSelected: paragraphTwo})
-  //   this.getExpertise()
-  // }
-
-  // handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   if (this.state.mediaId){
-  //     axios.post('/expertise/create', this.state)
-  //     .then((res) => {
-  //       this.getExpertise()
-  //     })
-  //     .catch((err) => console.log(err))
-  //   } else {
-  //     alert("Selectionnez une image.")
-  //   }
-  // }
-
   handleSubmit = (e) => {
     e.preventDefault()
     if(window.confirm("Voulez-vous valider les modifications ?")){
-      axios.put(`/expertise/edit/1`, this.state)
+      axios.put(`/expertise/edit/5`, this.state)
       .then((res) => alert("Modifications effectuées"))
       .catch((err) => alert("Erreur lors de la sauvegarde des modifications"))
       }
@@ -88,7 +55,11 @@ export default class Expertise extends Component {
   getExpertise = () => {
     axios.get('/expertise')
     .then((res) => {
-      this.setState({expertise: res.data.expertise})
+      this.setState({
+        paragraphOne: res.data.expertise[0].paragraphOne,
+        paragraphTwo: res.data.expertise[0].paragraphTwo,
+        mediaId: res.data.expertise[0].mediaId,
+      })
     })
     .catch((err) => console.log("Erreur lors de l'obtention de l'expertise"))
   }
@@ -105,7 +76,7 @@ export default class Expertise extends Component {
 
           <div className="MediaList">
 
-          <h3>Selectionnez une icône :</h3>
+          <p>Selectionnez un média :</p>
 
             {this.state.medias && 
             this.state.medias.map((media) => {
@@ -144,8 +115,10 @@ export default class Expertise extends Component {
               id="paragraphTwo"
               className="paragraphTwo"
               value={this.state.paragraphTwo}
-              onChange={(value) => this.handleChangeparagraphTwo(value)}
+              onChange={(value) => this.handleChangeParagraphTwo(value)}
             />
+
+            <p className="ImageSelected">Icône selectionnée : {this.state.mediaId} </p>
 
             {/* SUBMIT */}
 
@@ -155,118 +128,8 @@ export default class Expertise extends Component {
             >Modifier</button>
           
           </form>
-
-          {/* <form 
-            onSubmit={(e) => this.handleSubmit(e)} 
-            className="ExpertiseForm">
-
-            <h3>Création d'une expertise :</h3>
-
-            <label 
-              className="col-form-label"
-            >Premier paragraphe :
-            </label>
-
-            <textarea 
-              type="text" 
-              name="paragraphOne" 
-              value={this.state.paragraphOne}
-              onChange={(e) => this.handleChange(e)}
-              className="form-control" 
-              rows="5"
-              required 
-              ></textarea>
-
-            <label 
-              className="col-form-label"
-              >Second paragraphe :
-            </label>
-
-            <textarea 
-              type="text" 
-              name="paragraphTwo" 
-              value={this.state.paragraphTwo}
-              onChange={(e) => this.handleChange(e)}
-              className="form-control" 
-              rows="5"
-              required 
-              ></textarea>
-
-              <p className="ImageSelected">Image selectionnée : {this.state.mediaId} </p>
-              
-
-            <input 
-              type="submit" 
-              value="Créer une expertise" 
-              className="btn btn-outline-dark"
-              ></input>
-
-          </form> */}
-        </div>
-
-        {/* <hr/> */}
-
-        {/* {this.state.modal && 
-
-          <ExpertiseModal 
-            close={this.handleClick}
-            id={this.state.idSelected}
-            mediaId={this.state.mediaIdSelected}
-            paragraphOne={this.state.paragraphOneSelected} 
-            paragraphTwo={this.state.paragraphTwoSelected}
-          />
-
-        } */}
-        
-        {/* LISTE EXPERTISE */}
-
-        {/* <div className="ExpertiseList">
-          
-          <h3>Liste de l'expertise :</h3>
-
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">Premier paragraphe</th>
-                <th scope="col">Second paragraphe</th>
-                <th scope="col">Média</th>
-                <th scope="col">Modifier</th>
-                <th scope="col">Supprimer</th>
-              </tr>
-            </thead>
-
-            {this.state.expertise &&
-
-            this.state.expertise.map((expertise, index) => {
-              return (
-
-                <tbody key={index}>
-                  <tr>
-
-                    <th scope="row">{expertise.paragraphOne}</th>
-                    <td>{expertise.paragraphTwo}</td>
-                    <td>{expertise.mediaId}</td>
-                    <td><button 
-                      className="btn btn-outline-primary"
-                      onClick={() => this.handleClick(expertise.id,expertise.mediaId,expertise.paragraphOne, expertise.paragraphTwo)}>
-                        <i class="fas fa-pen"></i>
-                    </button></td>
-                    <td><button 
-                      className="btn btn-outline-danger"
-                      onClick={() => this.handleDelete(expertise.id)}>
-                        <i className="fas fa-trash-alt"></i>
-                    </button></td>
-
-                  </tr>
-                  
-                </tbody>
-
-              )
-            })}
-
-          </table>
-
-        </div> */}
+         
+        </div>      
 
       </div>
     )
--- frontend/src/components/Expertise/Expertise.js
+++ frontend/src/components/Expertise/Expertise.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import './Expertise.scss';
+import axios from 'axios'
 
 export default class Expertise extends Component {
+  state = {
+    expertise : []
+  }
+
+  componentDidMount = () => {
+    axios.get('/expertise/')
+    .then((res) => this.setState({expertise: res.data.expertise[0]}))
+    .catch((err) => {console.log(err)});
+  }
+  
+
   render() {
+    console.log(this.state.expertise)
     return (
       <div className="expertContainer" id="expertise">
         <h1 className="expertTitle">Expertise</h1>
-        <div className="expertDescription">
+        {/* <div className="expertDescription">
           <p className="expertParagraph">À l’ère du Mobile first, la maîtrise de l’approche contextuelle est décisive pour renforcer efficacement l’engagement d’une audience.</p>
           <p>Walkoo propose cette nouvelle approche, fondée sur les sciences du comportement et les dernières technologies de géolocalisation.</p>
             <hr />
@@ -16,8 +29,18 @@ export default class Expertise extends Component {
         </div>
         <div className="expertImage">
           <img src={require('./Illustration.png')} alt="Illustration" className="expertImage"/>
-        </div>        
+        </div>         */}
+        <div dangerouslySetInnerHTML={createMarkup(this.state.expertise.paragraphOne)} className="expertDescription"></div>
+        <hr/>
+        <div dangerouslySetInnerHTML={createMarkup(this.state.expertise.paragraphTwo)} className="expertSubtitle"></div>
+        <div className="expertImage">
+          <img src={`${this.state.expertise.image.url}`} alt="Illustration"/>
+        </div>   
       </div>
     )
   }
+}
+
+function createMarkup(stringyfiedHtml) {
+  return { __html: stringyfiedHtml };
 }
