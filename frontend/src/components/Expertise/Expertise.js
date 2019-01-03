066c8d53651991538b2354832df37e9398e90e38
Julie Gicquel <julie.gcql@gmail.com>
2019-01-03 17:16:30 +0100

    finish link back & front

--- frontend/src/components/Expertise/Expertise.js
+++ frontend/src/components/Expertise/Expertise.js
@@ -15,26 +15,15 @@ export default class Expertise extends Component {
   
 
   render() {
-    console.log(this.state.expertise)
+    console.log("jhcjhjd", this.state.expertise)
+    const imgUrl = this.state.expertise.image ? this.state.expertise.image.url : null
     return (
       <div className="expertContainer" id="expertise">
         <h1 className="expertTitle">Expertise</h1>
-        {/* <div className="expertDescription">
-          <p className="expertParagraph">À l’ère du Mobile first, la maîtrise de l’approche contextuelle est décisive pour renforcer efficacement l’engagement d’une audience.</p>
-          <p>Walkoo propose cette nouvelle approche, fondée sur les sciences du comportement et les dernières technologies de géolocalisation.</p>
-            <hr />
-        </div>
-        <div className="expertSubtitle">
-          <p>Walkoo conçoit des solutions intégrées qui associent la localisation de l’utilisateur dans son environnement (indoor & outdoor) et la scénarisation de notifications contextuelles pour l’informer ou l’assister en temps réel.</p>
-        </div>
-        <div className="expertImage">
-          <img src={require('./Illustration.png')} alt="Illustration" className="expertImage"/>
-        </div>         */}
         <div dangerouslySetInnerHTML={createMarkup(this.state.expertise.paragraphOne)} className="expertDescription"></div>
-        <hr/>
         <div dangerouslySetInnerHTML={createMarkup(this.state.expertise.paragraphTwo)} className="expertSubtitle"></div>
         <div className="expertImage">
-          <img src={`${this.state.expertise.image.url}`} alt="Illustration"/>
+          <img src={imgUrl} alt="Illustration"/>
         </div>   
       </div>
     )
