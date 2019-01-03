32c02dd3aa9464ebba42eff987519cb5388e0631
Julien <julien.bertocchini@gmail.com>
2019-01-03 09:36:27 +0100

    Remove comments

--- frontend/src/components/Technologie/Technologie.js
+++ frontend/src/components/Technologie/Technologie.js
@@ -11,7 +11,7 @@ export default class Technologie extends Component {
   state = {
     technologies:[]
 
-  }
+  };
 
   componentDidMount = () => {
       axios.get('/technology')
@@ -20,7 +20,7 @@ export default class Technologie extends Component {
         this.setState({technologies: res.data.technology})
       })
       .catch((err) => console.log("Erreur lors de l'obtention des realisations"))
-    }
+    };
 
 
   render() {
@@ -75,52 +75,3 @@ export default class Technologie extends Component {
     )
   }
 }
-
-
-{/* //     return (
-//       <div className="technologie_container" id="technologie">
-//         <div className="technologie-firstpart">
-//          <h1>Technologie</h1>
-//          <p>La solution numérique développée par Walkoo se compose d’une plateforme personnalisée que vous administrez à partir de comptes dédiés et d’une application mobile spécialement désignée, téléchargeable sur smartphone.</p>
-//         </div>
-//         <h3>Les avantages de la solution Walkoo</h3>
-//         <div className="technologie-secondpart">
-//          <div className="grid_container_avantage">
-//           <div className="avantage avantage1">
-//             <div className='title1'>
-//               <img src={require('./link.png')} alt="logo Link"/>
-//               <h6>Lien direct avec <br/>
-//               votre audience</h6>
-//             </div>
-//             <p className='text1'>{text}</p>
-//           </div>
-//           <div className="avantage avantage2">
-//             <div className='title2'>
-//               <img src={require('./notification.png')} alt="logo notification"/>
-//               <h6>Contextualisation <br/>
-//               des notifications</h6>
-//             </div>            
-//             <p className='text2'>{text}</p>
-//           </div>
-//           <div className="avantage avantage1">
-//             <div className='title1'>
-//               <img src={require('./ux.png')} alt="logo UX"/>
-//               <h6>Ux <br/>
-//                 Design</h6>
-//             </div>
-//             <p className='text1'>{text}</p>
-//           </div>
-//           <div className="avantage avantage2">
-//             <div className='title2'>
-//               <img src={require('./performance.png')} alt="logo performance"/>
-//               <h6>Notre <br/>
-//                 Efficacité</h6>
-//             </div>
-//             <p className='text2'>{text}</p>
-//           </div>
-//          </div>
-//         </div>
-//       </div>
-//     )
-//   }
-// } */}
