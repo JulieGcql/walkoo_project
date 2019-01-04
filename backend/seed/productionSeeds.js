7d4b011f0d24c20b46989c23080b6dc1d1905d62
Hugo Arvis <arvishugo@hotmail.fr>
2019-01-04 16:31:31 +0100

    accueil and mediasalons seeds

--- backend/seed/productionSeeds.js
+++ backend/seed/productionSeeds.js
@@ -1,11 +1,11 @@
 const models = require('../models');
 const Home = models.Home;
 
-const faker = require('faker');
+
 
 Home.create({
-    catchPhrase: faker.lorem.sentence(),
-    subtitle: faker.lorem.sentence()
+    
+    subtitle: "Walkoo combine la géolocalisation et les sciences du comportement pour renforcer l’engagement de l’utilisateur en maîtrisant le sens du contexte"
 })
 .then((createdHome) => console.log(createdHome))
 .catch((err) => console.log(err))
