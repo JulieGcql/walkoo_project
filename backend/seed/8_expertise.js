f03872cbd1233f763f13d8fec7397243723a857d
Julie Gicquel <julie.gcql@gmail.com>
2019-01-04 17:05:52 +0100

    correct seed

--- backend/seed/8_expertise.js
+++ backend/seed/8_expertise.js
@@ -1,14 +1,14 @@
 const models = require('../models');
 const Expertise = models.Expertise;
 
-Expertise.create([
+Expertise.create(
     {
         paragraphOne: "<p>Fondée sur les connaissances issues des sciences du comportement, l’approche contextuelle permet d’engager une audience de façon réellement efficace en tenant compte de nombreux paramètres : profil et favoris de l’utilisateur, position géographique, date, moment de la journée, qualité de l’air, actualité… Délivrées dans le bon contexte, informations et notifications ne peuvent qu’être bien accueillies !</p>",
         paragraphTwo: "<p>Vos contenus méritent un expert du contexte. Nous vous accompagnons depuis la définition de votre projet jusqu’à son déploiement. Diagnostic, accompagnement personnalisé, évaluation. Bienvenue chez WALKOO.</p>",
         mediaId: 2,
     },
 
-])
+)
     .then((createdExpertise) => {
         console.log(createdExpertise)
     })
