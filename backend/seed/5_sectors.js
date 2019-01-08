34213b5046860309ad5641854ae221cf495c9cdb
Julien <julien.bertocchini@gmail.com>
2019-01-08 09:22:54 +0100

    Put text editor in technologie

--- backend/seed/5_sectors.js
+++ backend/seed/5_sectors.js
@@ -8,7 +8,7 @@ Sector.bulkCreate([
     description: "Qu’il s’agisse de patrimoine bâti, naturel, immatériel ou de création contemporaine, la personnalisation est une tendance forte de la médiation culturelle.Grâce à l’approche contextuelle, Walkoo est en mesure de : - créer un flux de contenus localisés et actualisés, des guides d’expositions ou encore des parcours sonores dans la ville - personnaliser la forme des articles pour différents publics (audio, vidéo…) - imaginer de nouvelles formes de diffusion inspirées des nouveaux usages et créer des chatbots. WALKOO scénarise vos contenus pour renouveler l’intérêt de vos publics actuels et conquérir ceux de demain."
   },
   {
-    mediaId: 8,
+    mediaId: 18,
     title: "Evenements",
     description: "Qu’il s’agisse de festivals de street art, de rendez-vous gastronomiques, de grands événements culturels ou sportifs, d’animation d’un centre-ville historique ou de la Smart City, la personnalisation est une tendance forte de l’événementiel. Grâce à l’approche contextuelle, Walkoo est en mesure de : - créer un flux de contenus localisés et actualisés, des guides ou encore des parcours dans la ville - personnaliser la forme des articles pour différents publics (audio, vidéo…) - imaginer de nouvelles formes de diffusion inspirées des nouveaux usages et créer des chatbots. WALKOO permet de localiser les contenus de vos événements tout en tenant compte des préférences de chaque utilisateur et des éléments déterminants de son environnement (météo, qualité de l’air, flux d’actualités…). Délivrées au bon endroit et au bon moment, l’information s'adapte aux attentes de chaque participant pour renouveler l’intérêt de vos publics actuels et conquérir ceux de demain. "
   },
