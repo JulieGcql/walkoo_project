2953e44ef2782f9d230439c46f2847c6183f98a2
David <eth3rnit3@outlook.fr>
2019-01-15 17:44:29 +0100

    update frontend

--- backend/client/build/precache-manifest.dd6101ab5414821602a0b61f4f685cb2.js
+++ backend/client/build/precache-manifest.dd6101ab5414821602a0b61f4f685cb2.js
@@ -4,12 +4,12 @@ self.__precacheManifest = [
     "url": "/static/media/app-store.96417f67.png"
   },
   {
-    "revision": "f97a9a1800f71228df52",
-    "url": "/static/css/main.f6c1eb61.chunk.css"
+    "revision": "42c5886e842d94f37dee",
+    "url": "/static/css/main.04bf0637.chunk.css"
   },
   {
-    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
-    "url": "/static/media/slick.b7c9e1e4.woff"
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
   },
   {
     "revision": "95de0cab46757262c0d4",
@@ -24,27 +24,27 @@ self.__precacheManifest = [
     "url": "/static/media/more.bec7a80c.png"
   },
   {
-    "revision": "f97a9a1800f71228df52",
-    "url": "/static/js/main.f97a9a18.chunk.js"
+    "revision": "42c5886e842d94f37dee",
+    "url": "/static/js/main.42c5886e.chunk.js"
   },
   {
-    "revision": "f97e3bbf73254b0112091d0192f17aec",
-    "url": "/static/media/slick.f97e3bbf.svg"
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
   },
   {
     "revision": "ced611daf7709cc778da928fec876475",
     "url": "/static/media/slick.ced611da.eot"
   },
   {
-    "revision": "d41f55a78e6f49a5512878df1737e58a",
-    "url": "/static/media/slick.d41f55a7.ttf"
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
   },
   {
     "revision": "95de0cab46757262c0d4",
     "url": "/static/css/1.24d9b2c1.chunk.css"
   },
   {
-    "revision": "dcf8da4f4922d1d58c5580257bd88b8d",
+    "revision": "bf5c3f8d7335711640f9f8fcbd7159bb",
     "url": "/index.html"
   }
 ];
