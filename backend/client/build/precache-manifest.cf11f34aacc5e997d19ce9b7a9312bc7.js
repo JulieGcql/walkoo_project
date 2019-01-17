15a38256907864aad9bc4ac7ad1ec16fa7b1d9c7
David <eth3rnit3@outlook.fr>
2019-01-17 12:36:21 +0100

    fix bugs

---
+++ backend/client/build/precache-manifest.cf11f34aacc5e997d19ce9b7a9312bc7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "96417f67c27886477fa8ae1863c2ee9c",
+    "url": "/static/media/app-store.96417f67.png"
+  },
+  {
+    "revision": "7c4549aa00cc797f2687",
+    "url": "/static/css/main.b18c8417.chunk.css"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "50fbbce524f9a8ad702e",
+    "url": "/static/js/1.50fbbce5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bec7a80cdd0f2ee2d752d3a750bbf600",
+    "url": "/static/media/more.bec7a80c.png"
+  },
+  {
+    "revision": "7c4549aa00cc797f2687",
+    "url": "/static/js/main.7c4549aa.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "50fbbce524f9a8ad702e",
+    "url": "/static/css/1.407d7735.chunk.css"
+  },
+  {
+    "revision": "649306d3afee25cac70991cfc27f426b",
+    "url": "/index.html"
+  }
+];
--- backend/client/build/service-worker.js
+++ backend/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd6101ab5414821602a0b61f4f685cb2.js"
+  "/precache-manifest.cf11f34aacc5e997d19ce9b7a9312bc7.js"
 );
 
 workbox.clientsClaim();
